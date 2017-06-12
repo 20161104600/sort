9512ec0ea758c805a986239b1760a440bc426b65
20161104600 <1428373962@qq.com>
2017-06-12 11:51:00 +0800

    maopao
    有错误

--- sort/main.c
+++ sort/main.c
@@ -7,9 +7,28 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+void Bubble(int num[],int n)
+{
+    int i,j;
+    for(i=0;i<n;i++)
+    {
+        for(j=0;i+j<n-1;j++)
+        {
+            if(num[j]>num[j+1])
+            {
+                int m = num[j];
+                num[j] = num[j+1];
+                num[j+1] = m;
+            }
+        }
+        printf("%d ",num[i]);
+    }
+    return;
+}
+int main()
+{
+    int num[10]={11,2,60,45,3,6,65,12,98,23};
+    Bubble(num,10);
     return 0;
 }
+   
